b68380666dbefa34a6bf497e2068cf5e48d9e6d9
AhmadMD16 <103050416+AhmadMD16@users.noreply.github.com>
2022-08-12 23:09:35 +0200

    Initiating commit

---
+++ YouTubeVideoDownloader.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+
+def download_video():
+    video = YouTube(url.get())
+    video = video.streams.get_highest_resolution()
+    print(video.title)
+    video.download(r'directory')
+    messagebox.showinfo("Success", "Video has been successfully downloaded")
+
+master = tk.Tk()
+master.geometry("450x100")
+master.iconbitmap("YT.ico")
+master.title("Youtube Video Downloader")
+tk.Label(master, text="   URL:").grid(row=0)
+url = tk.Entry(master)
+url = Entry(master, width=60, borderwidth=5)
+url.grid(row=0,column=0, columnspan=3, padx=10, pady=10)
+url.grid(row=0, column=1)
+tk.Button(master, text='Quit', command=master.quit).grid(row=4, column=2, sticky=tk.W, pady=4)
+tk.Button(master, text='Download', command=download_video).grid(row=4, column=3, sticky=tk.W, pady=4)
+
+master.mainloop()
+
